75dc8a58aafe649d120c99854ef97f444c4522b6
Tristan Jacobs <tristan@iamt.nl>
2017-07-11 21:47:49 +0200

    basic routing for poll and post handling

--- Week3-PE-BrowserTech/server.js
+++ Week3-PE-BrowserTech/server.js
@@ -4,52 +4,27 @@ const request           = require('request');
 const bodyParser        = require('body-parser');
 const app               = express();
 const compression       = require('compression');
-const urlencodedParser  = bodyParser.urlencoded({extended: false});
-const exampleData       = require('./data.json');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(urlencodedParser);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(compression())
 app.set('port', (8080));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        user_query: req.query,
-        items: null
-    });
+    res.render('index', {} );
 });
 
-app.post('/', urlencodedParser, (req, res) => {
-
-    String.prototype.capitalize = function () {
-        return this.charAt(0).toUpperCase() + this.slice(1);
-    };
-
-    var people = exampleData;
-    var userQuery = req.body.query;
-    var formattedQuery = userQuery.capitalize();
-    var results = [];
-
-    for (var i = 0; i < people.length; i++) {
-        if (people[i].first_name.includes(formattedQuery) || people[i].last_name.includes(formattedQuery)) {
-            results.push(people[i]);
-        }
-    }
-
-    res.render('index', {
-        user_query: formattedQuery,
-        items: results
-    });
+app.post('/', (req, res) => {
+  let fields = req.body;
+  console.log(fields);
+  res.redirect('/results');
 });
 
-app.get('/person/:id', (req, res) => {
-    var id = req.params.id;
-    var person = exampleData.find(result => {
-        return result.id == id;
-    });
-    res.render('details', {"person": person});
-});
+app.get('/results', (req, res) => {
+  res.render('results');
+})
 
 app.listen(app.get('port'), () => {
     console.log('App listening on: http://localhost:8080');
